8d04400d0762508bb31f4dff0c759b6e4818317c
DevAhmedZiad <ahmed2588798@gmail.com>
2023-04-25 00:23:34 +0300

    profile

--- backend/controller/profileController.js
+++ backend/controller/profileController.js
@@ -1,10 +1,10 @@
 const asyncHandler = require('express-async-handler')
-
 const User = require('../model/userModel')
 
+// @desc  Get User profile
+// @route GET api/profile/getProfile
+// @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    
-    console.log(req.user._id)
 
     const user = await User.findById(req.user._id)
     if (!user) {
@@ -20,7 +20,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 
     res.status(200).json({
-        _id:user._id,
+        _id: user._id,
         name: user.name,
         email: user.email,
         phone: user.phone,
@@ -32,4 +32,26 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 
 })
-module.exports = { getUserProfile }
+
+// @desc  Update User profile
+// @route PUT api/profile/editProfile
+// @access Private
+const editProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    if (!user) {
+
+        res.status(400).json({
+            statusCode: 400,
+            message: "User not exists",
+            details: {
+                user: 'User not exists'
+            },
+        });
+        throw new Error('User not exists')
+    }
+
+    const updatedProfile = await User.findByIdAndUpdate(req.user._id, req.body, { new: true })
+    res.status(200).json(updatedProfile)
+})
+
+module.exports = { getUserProfile, editProfile }
