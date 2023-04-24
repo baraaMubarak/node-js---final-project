4b0894eccf4599d9a0ff7deaf6f3cbda24a0f193
Nassem <nassemmubarak@gmail.com>
2023-04-24 23:31:50 +0200

    chang export news model

--- backend/scraping/newsWalla.js
+++ backend/scraping/newsWalla.js
@@ -3,16 +3,28 @@ const {translateText} = require('../translator')
 getNews = async(url)=>{
     const response = await fetch(url)
     const body = await response.text()
-    const $ = await cheerio.load(body)
+    const $ =  cheerio.load(body)
 
-    const l = $('.category-five-articles-small-item').map(async(index,el)=>{
+     $('.category-five-articles-small-item').map(async(index,el)=>{
         const imageUrl = $(el).find('.category-five-articles-small-item-img img').attr('src')
         const title = $(el).find('.category-five-articles-text-wrap .category-five-articles-small-item-title').text()
         const link = $(el).find('a').attr('href')
-        console.log(link);
+        await getDetailsNews(link)
+        // console.log(link);
         
     })
     // console.log(l);
+}
+const getDetailsNews = async(url)=>{
+    const response = await fetch(url)
+    const body = await response.text()
+    const $ =  cheerio.load(body)
+    let textBody;
+    $('.article-body').map((index,el)=>{
+        textBody += $(el).find('p').text()
+    })
+    console.log(textBody);
+
 }
 const url = `https://www.maariv.co.il/news/politics`
 getNews(url)
