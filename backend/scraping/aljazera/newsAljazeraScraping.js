0ad95e968e35ab5377a397ae8849949375f181f1
DevAhmedZiad <ahmed2588798@gmail.com>
2023-04-26 11:32:22 +0300

    scraping aljazera

---
+++ backend/scraping/aljazera/newsAljazeraScraping.js
@@ -0,0 +1,89 @@
+const cheerio = require("cheerio")
+const { Policy, Economy, Sports } = require('../../model/newsModel')
+const getAljazeraNews = async (url, type) => {
+    try {
+        const response = await fetch(url)
+        const body = await response.text()
+        const $ = cheerio.load(body)
+
+        $('.gc').map(async (i, el) => {
+            const title = $(el).find('.gc__title').text()
+            const body = $(el).find('.gc__body-wrap').text()
+            const date = $(el).find('.screen-reader-text').text()
+            const imageUrl = url + $(el).find('.gc__image ').attr('src')
+            const link = url + $(el).find('.u-clickable-card__link').attr('href')
+
+            if (type == 'policy') {
+                const newsExit = await Policy.findOne({ link });
+                if (!newsExit) {
+                    await getDetailsNews(link).then(async (data) => {
+                        await Policy.create({
+                            title: title,
+                            body: body,
+                            date: date,
+                            imageUrl: imageUrl,
+                            link: link,
+                            Comment: [],
+                            author: 'aljazera.net',
+                            details: data.details
+                        })
+                    })
+                }
+            } else if (type == 'economy') {
+                const newsExit = await Economy.findOne({ link });
+                if (!newsExit) {
+                    await getDetailsNews(link).then(async (data) => {
+                        await Economy.create({
+                            title: title,
+                            body: body,
+                            date: date,
+                            imageUrl: imageUrl,
+                            link: link,
+                            Comment: [],
+                            author: 'aljazera.net',
+                            details: data.details
+                        })
+                    })
+                }
+            } else if (type == 'sport') {
+                const newsExit = await Sports.findOne({ link });
+                if (!newsExit) {
+                    await getDetailsNews(link).then(async (data) => {
+                        await Sports.create({
+                            title: title,
+                            body: body,
+                            date: date,
+                            imageUrl: imageUrl,
+                            link: link,
+                            Comment: [],
+                            author: 'aljazera.net',
+                            details: data.details
+                        })
+                    })
+                }
+            }
+
+        })
+
+    } catch (error) {
+
+        console.log(error);
+    }
+}
+
+
+const getDetailsNews = async (url) => {
+    const response = await fetch(url);
+    const body = await response.text();
+    const $ = cheerio.load(body);
+    let details
+    $('.wysiwyg--all-content').map(async (i, el) => {
+        details += $(el).find('p').text()
+        // console.log(details)
+    })
+    return { details: details }
+}
+
+module.exports = {
+    getAljazeraNews
+}
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,37 @@
 const express = require('express')
-const {errorHandler} = require('./middleWare/errorMiddleWare')
+const { errorHandler } = require('./middleWare/errorMiddleWare')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db.js')
 const port = process.env.PORT || 5000
-const {newsWallaPolicyScraping} = require('./scraping/newsWallaPolicyScraping')
-const {newsWallaSportScraping} = require('./scraping/newsWallaSportScraping')
+const { newsWallaPolicyScraping } = require('./scraping/newsWallaPolicyScraping')
+const { newsWallaSportScraping } = require('./scraping/newsWallaSportScraping')
+const {  getAljazeraNews } = require('./scraping/aljazera/newsAljazeraScraping')
 const app = express()
 connectDB()
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use('/api/users', require('./routes/userRouters.js'))
 app.use('/api/news', require('./routes/newsRouters.js'))
 app.use('/api/profile', require('./routes/profileRouters.js'))
+
+//walla
 const urlPolicy = `https://www.maariv.co.il/news/politics`
 newsWallaPolicyScraping(urlPolicy)
 const urlSport = `https://sport1.maariv.co.il/world-soccer/`
 newsWallaSportScraping(urlSport)
-app.use(errorHandler)
+
+
+//aljazera
+const urlSportAljazera = 'https://www.aljazeera.net/sport/'
+getAljazeraNews(urlSportAljazera, 'sport')
+const urlPolicyAljazera = 'https://www.aljazeera.net/politics/'
+getAljazeraNews(urlPolicyAljazera, 'policy')
+const urlEconomicAljazera = 'https://www.aljazeera.net/ebusiness/'
+getAljazeraNews(urlEconomicAljazera, 'economy')
+
+
+    app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port http://localhost:${port}`))
-const {translateText} = require('./translator.js');
+const { translateText } = require('./translator.js');
