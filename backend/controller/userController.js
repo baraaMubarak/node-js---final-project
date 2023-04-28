7b4918ab00bc46f1b8cb9fb3a1944952ddaf1d22
DevAhmedZiad <ahmed2588798@gmail.com>
2023-04-28 14:36:39 +0300

    test

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,11 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const { sendEmail } = require('../core_modules/emailVerification')
 const User = require('../model/userModel')
-const {  
+const {
     nameValidate,
     emailValidate,
     passwordValidate,
-    phoneNumberValidate 
+    phoneNumberValidate
 } = require('../core_modules/validation')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
@@ -19,176 +19,176 @@ const jwt = require('jsonwebtoken')
 const registerUser = asyncHandler(async (req, res) => {
     const code = generateVerificationCode();
     isError = false;
-  const { name, email, password, phone } = req.body;
-      let details = {}
-      //validate name
-      const nameError = nameValidate(!name?'':name);
-      if(nameError){
+    const { name, email, password, phone } = req.body;
+    let details = {}
+    //validate name
+    const nameError = nameValidate(!name ? '' : name);
+    if (nameError) {
         isError = true
         details.name = nameError;
     }
     //validate email
-    const emailError = emailValidate(!email?'':email)
-    if(emailError){
-        isError=true;
+    const emailError = emailValidate(!email ? '' : email)
+    if (emailError) {
+        isError = true;
         details.emailError = emailError
     }
     console.log(details);
     //validate password
-    const passwordError = passwordValidate(!password?'':password)
-    if(passwordError){
+    const passwordError = passwordValidate(!password ? '' : password)
+    if (passwordError) {
         isError = true;
         details.password = passwordError
     }
     //validate Phone number
-    const phoneError = phoneNumberValidate(!phone?'':phone)
-    if(phoneError){
+    const phoneError = phoneNumberValidate(!phone ? '' : phone)
+    if (phoneError) {
         isError = true
         details.phone = phoneError
     }
-        if(isError){
-            res.status(400).json({
+    if (isError) {
+        res.status(400).json({
             statusCode: 400,
             message: "Enter require data",
             details: details,
+        });
+        throw new Error("Enter require data");
+    } else {
+        const emailExists = await User.findOne({ email })
+        if (emailExists) {
+            res.status(400).json({
+                statusCode: 400,
+                message: "Email already exists",
+                details: {
+                    user: 'Email already exists'
+                },
             });
-            throw new Error("Enter require data");
-        }else{
-            const emailExists = await User.findOne({email})
-            if(emailExists){
-                res.status(400).json({
-                    statusCode: 400,
-                    message: "Email already exists",
-                    details: {
-                        user:'Email already exists'
-                    },
-                    });
-                throw new Error('Email already exists')
-            }
-            const phoneExists = await User.findOne({phone})
-            if(phoneExists){
-                res.status(400).json({
-                    statusCode: 400,
-                    message: "Phone number already exists",
-                    details: {
-                        user:'Phone number already exists'
-                    },
-                    });
-                throw new Error('Phone number already exists')
-            }
-
-            //hash password
-            const salt = await bcrypt.genSalt(10)
-            const hashedPassword = await bcrypt.hash(password,salt)
-            //create user
-            const user =await User.create({
-                name : name,
-                email:email,
-                phone:phone,
-                verificationCode:code,
-                verifyEmail:false,
-                password:hashedPassword,
-                // image: {
-                //     filename: req.file.originalname,
-                //     contentType: req.file.mimetype,
-                //     data: req.file.buffer,
-                //     userImage:req.file.path
-                // },
-            })
-            if(!user){
-                res.status(400).json({
-                    statusCode: 400,
-                    message: "Not successful",
-                    details: {
-                        message:'Not successful'
-                    },
-                    });
-                throw new Error('Not successful')
-            }
-            if(!await sendEmail(user.email,code,user.name)){
-                res.status(400).json({
-                    statusCode: 400,
-                    message: "Not successful",
-                    details: {
-                        message:'Not successful'
-                    },
-                    });
-                throw new Error('Not successful')
-            }
-            res.status(200).json({ 
-                statusCode: 200,
-                message: "Thank you for registering with bnmaNews! We have sent a verification code to your email. Please enter the code on our website within the next 30 minutes to complete your registration.",
-                user:{
-                    _id: user.id,
-                    name:user.name,
-                    email:user.email,
-                    phone:user.phone,
-                    verifyEmail:user.verifyEmail,
-                    // image: 'http://localhost:8000/'+user.image.userImage,
-                    token:generateToken(user._id),
-                }
-                }); 
+            throw new Error('Email already exists')
         }
+        const phoneExists = await User.findOne({ phone })
+        if (phoneExists) {
+            res.status(400).json({
+                statusCode: 400,
+                message: "Phone number already exists",
+                details: {
+                    user: 'Phone number already exists'
+                },
+            });
+            throw new Error('Phone number already exists')
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+        //create user
+        const user = await User.create({
+            name: name,
+            email: email,
+            phone: phone,
+            verificationCode: code,
+            verifyEmail: false,
+            password: hashedPassword,
+            // image: {
+            //     filename: req.file.originalname,
+            //     contentType: req.file.mimetype,
+            //     data: req.file.buffer,
+            //     userImage:req.file.path
+            // },
+        })
+        if (!user) {
+            res.status(400).json({
+                statusCode: 400,
+                message: "Not successful",
+                details: {
+                    message: 'Not successful'
+                },
+            });
+            throw new Error('Not successful')
+        }
+        if (!await sendEmail(user.email, code, user.name)) {
+            res.status(400).json({
+                statusCode: 400,
+                message: "Not successful",
+                details: {
+                    message: 'Not successful'
+                },
+            });
+            throw new Error('Not successful')
+        }
+        res.status(200).json({
+            statusCode: 200,
+            message: "Thank you for registering with bnmaNews! We have sent a verification code to your email. Please enter the code on our website within the next 30 minutes to complete your registration.",
+            user: {
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                verifyEmail: user.verifyEmail,
+                // image: 'http://localhost:8000/'+user.image.userImage,
+                token: generateToken(user._id),
+            }
+        });
+    }
 });
 
 
 //@desc    Authenticate a user
 //@route   POST /api/users/login
 //@access  Public 
-const loginUser = asyncHandler(async(req,res)=>{
+const loginUser = asyncHandler(async (req, res) => {
     let details = {}
-    const { email , password } = req.body;
-    if((!email||!password)){
+    const { email, password } = req.body;
+    if ((!email || !password)) {
         let details = {}
-        if(!email){
+        if (!email) {
             details.email = 'Please enter your email.'
         }
-        if(!password){
+        if (!password) {
             details.password = 'Please enter your password.'
         }
         res.status(400).json({
             statusCode: 400,
             message: "Enter require data",
             details: details,
-            });
-            throw new Error("Enter require data")
+        });
+        throw new Error("Enter require data")
     }
     //validate email
-    const emailError = emailValidate(!email?'':email)
-    if(emailError){
+    const emailError = emailValidate(!email ? '' : email)
+    if (emailError) {
         details.emailError = emailError
         res.status(400).json({
             statusCode: 400,
             message: emailError,
             details: details,
         });
-            throw new Error(emailError)
+        throw new Error(emailError)
     }
-    const user = await User.findOne({email})
-    if(!user){
+    const user = await User.findOne({ email })
+    if (!user) {
         res.status(400).json({
             statusCode: 400,
             message: 'User not found',
-            details: {user:"User not found"},
-            });
-            throw new Error('User not found')
+            details: { user: "User not found" },
+        });
+        throw new Error('User not found')
     }
-    if(!await bcrypt.compare(password,user.password)){
+    if (!await bcrypt.compare(password, user.password)) {
         res.status(400).json({
             statusCode: 400,
             message: 'password not correct',
-            details: {user:"password not correct"},
-            });
-            throw new Error('password not correct')
+            details: { user: "password not correct" },
+        });
+        throw new Error('password not correct')
     }
     res.status(201).json({
         _id: user.id,
-        name:user.name,
-        email:user.email,
-        phone:user.phone,
-        verifyEmail:user.verifyEmail,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        verifyEmail: user.verifyEmail,
         // image: 'http://localhost:8000/'+user.image.userImage,
-        token:generateToken(user._id),
+        token: generateToken(user._id),
     })
 })
 
@@ -198,69 +198,69 @@ const loginUser = asyncHandler(async(req,res)=>{
 //@route   POST /api/users/verifyEmail
 //@access  Private 
 const emailVerification = asyncHandler(
-    async(req,res)=>{
+    async (req, res) => {
         const { code } = req.body
-        if(!code){
+        if (!code) {
             res.status(400).json({
                 statusCode: 400,
                 message: "Error: enter code.",
                 details: {
-                    message:'Error: enter code.'
+                    message: 'Error: enter code.'
                 },
-                });
+            });
             throw new Error('Error: enter code.')
         }
-        if(!(Number.isInteger(parseInt(code))&&parseInt(code).toString().length===4)){
+        if (!(Number.isInteger(parseInt(code)) && parseInt(code).toString().length === 4)) {
             res.status(400).json({
                 statusCode: 400,
                 message: "Error: Code must contain four integers.",
                 details: {
-                    message:'Error: Code must contain four integers.'
+                    message: 'Error: Code must contain four integers.'
                 },
-                });
+            });
             throw new Error('Error: Code must contain four integers.')
         }
-        const {id,verificationCode} = await User.findById(req.user._id)
-          if(!id){
-              res.status(400).json({
-                  statusCode: 400,
-                  message: 'User not found',
-                  details: {user:"User not found"},
-                  });
-                  throw new Error('User not found')
-          }
-        console.log(!(verificationCode==code));
-        if(!(verificationCode==code)){
+        const { id, verificationCode } = await User.findById(req.user._id)
+        if (!id) {
+            res.status(400).json({
+                statusCode: 400,
+                message: 'User not found',
+                details: { user: "User not found" },
+            });
+            throw new Error('User not found')
+        }
+        console.log(!(verificationCode == code));
+        if (!(verificationCode == code)) {
             res.status(400).json({
                 statusCode: 400,
                 message: "Your verification code is invalid. Please try again.",
                 details: {
-                    message:'Your verification code is invalid. Please try again.'
+                    message: 'Your verification code is invalid. Please try again.'
                 },
-                });
+            });
             throw new Error('Your verification code is invalid. Please try again.')
         }
-        const user = await User.findByIdAndUpdate(id,{
-            verifyEmail:true
+        const user = await User.findByIdAndUpdate(id, {
+            verifyEmail: true
         })
-        if(!user){
+        if (!user) {
             res.status(400).json({
                 statusCode: 400,
                 message: "Not successfully Please try again.",
                 details: {
-                    message:'Not successfully Please try again.'
+                    message: 'Not successfully Please try again.'
                 },
-                });
+            });
             throw new Error('Not successfully Please try again.')
         }
         res.status(201).json({
             _id: user.id,
-            name:user.name,
-            email:user.email,
-            phone:user.phone,
-            verifyEmail:true,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            verifyEmail: true,
             // image: 'http://localhost:8000/'+user.image.userImage,
-            token:generateToken(user._id),
+            token: generateToken(user._id),
         })
     }
 )
@@ -269,49 +269,49 @@ const emailVerification = asyncHandler(
 //@route   GET /api/users/verifyEmail
 //@access  Private 
 const reSendEmailVerification = asyncHandler(
-    async(req,res)=>{
+    async (req, res) => {
         const user = await User.findById(req.user._id)
-     if(!user){
-         res.status(400).json({
-             statusCode: 400,
-             message: 'User not found',
-             details: {user:"User not found"},
-             });
-             throw new Error('User not found')
-     }
-     const code = generateVerificationCode();
-     const userUpdate = await User.findByIdAndUpdate(user._id,{
-        verificationCode:code,
-    })
-    if(!userUpdate){
-        res.status(400).json({
-            statusCode: 400,
-            message: "Not successfully Please try again.",
-            details: {
-                message:'Not successfully Please try again.'
-            },
+        if (!user) {
+            res.status(400).json({
+                statusCode: 400,
+                message: 'User not found',
+                details: { user: "User not found" },
             });
-        throw new Error('Not successfully Please try again.')
-    }
-        if(!await sendEmail(user.email,code,user.name)){
+            throw new Error('User not found')
+        }
+        const code = generateVerificationCode();
+        const userUpdate = await User.findByIdAndUpdate(user._id, {
+            verificationCode: code,
+        })
+        if (!userUpdate) {
+            res.status(400).json({
+                statusCode: 400,
+                message: "Not successfully Please try again.",
+                details: {
+                    message: 'Not successfully Please try again.'
+                },
+            });
+            throw new Error('Not successfully Please try again.')
+        }
+        if (!await sendEmail(user.email, code, user.name)) {
             res.status(400).json({
                 statusCode: 400,
                 message: "Not successful",
                 details: {
-                    message:'Not successful'
+                    message: 'Not successful'
                 },
-                });
+            });
             throw new Error('Not successful')
         }
         res.status(201).json({
-            message:'Successfully'
+            message: 'Successfully'
         })
     }
 )
 
 //get token user
-const generateToken = (id)=>{
-    return jwt.sign({ id },process.env.JWT_SECRET||'abc123',{
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET || 'abc123', {
         expiresIn: '30d'
     })
 }
@@ -320,16 +320,16 @@ const generateVerificationCode = () => {
     let code = '';
     const possibleDigits = '123456789'; // exclude '0'
     for (let i = 0; i < 4; i++) {
-      code += possibleDigits.charAt(Math.floor(Math.random() * possibleDigits.length));
+        code += possibleDigits.charAt(Math.floor(Math.random() * possibleDigits.length));
     }
     return code;
-  };
+};
 
 
 
-  module.exports = {
+module.exports = {
     registerUser,
     loginUser,
     emailVerification,
     reSendEmailVerification
-  }
+}
--- backend/model/newsModel.js
+++ backend/model/newsModel.js
@@ -40,8 +40,8 @@ const newsSchema = mongoose.Schema({
     timestamps: true
 })
 
-module.exports ={
-   Policy : mongoose.model('Policy', newsSchema),
-   Economy :  mongoose.model('Economy', newsSchema),
-   Sports :  mongoose.model('Sports', newsSchema)
+module.exports = {
+    Policy: mongoose.model('Policy', newsSchema),
+    Economy: mongoose.model('Economy', newsSchema),
+    Sports: mongoose.model('Sports', newsSchema)
 }
