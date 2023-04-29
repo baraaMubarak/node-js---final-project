b47c880d33750884e57ca7975a65211082e633a3
praam <praa.m.2002@gmail.com>
2023-04-29 21:01:29 +0300

    for deployment - test error on errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,6 +8,4 @@ const errorHandler = (err, req, res, next) => {
         // stack: process.env.NODE_ENV === 'production' ? null : err.stack
       });
 }
-module.exports = {
-    errorHandler
-}
+module.exports = errorHandler
