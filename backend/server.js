f5802fa561e75f79f58ae3bb3d3873cdd7fd0f7a
praam <praa.m.2002@gmail.com>
2023-04-30 00:21:03 +0300

    for deployment - test error on errorMiddleware.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000
 const app = express()
 
 connectDB()
+
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))
 app.use(express.urlencoded({ extended: false }))
