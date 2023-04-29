7f904bd5357f17afef2f5c6df17c30074c0dfee1
praam <praa.m.2002@gmail.com>
2023-04-29 20:59:10 +0300

    for deployment - test error on errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,13 +1,13 @@
-// const errorHandler = (err, req, res, next) => {
-//     const statusCode = res.statusCode ? res.statusCode : 500
-//     res.status(statusCode)
-//     res.json({
-//         statusCode: statusCode,
-//         message: err.message,
-//         details: err.details,
-//         // stack: process.env.NODE_ENV === 'production' ? null : err.stack
-//       });
-// }
-// module.exports = {
-//     errorHandler
-// }
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500
+    res.status(statusCode)
+    res.json({
+        statusCode: statusCode,
+        message: err.message,
+        details: err.details,
+        // stack: process.env.NODE_ENV === 'production' ? null : err.stack
+      });
+}
+module.exports = {
+    errorHandler
+}
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const { errorHandler } = require('./middleWare/errorMiddleWare')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db.js')
 const port = process.env.PORT || 5000
 const app = express()
 connectDB()
-
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))
 app.use(express.urlencoded({ extended: false }))
@@ -14,6 +14,7 @@ app.use('/api/profile', require('./routes/profileRouters.js'))
 
 
 
+app.use(errorHandler)
 
 
 app.listen(port, () => console.log(`Server started on port http://localhost:${port}`))
