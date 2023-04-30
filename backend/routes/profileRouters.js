b706ce60ec1bb5b896017a07a8a01d60c71e8bf7
praam <praa.m.2002@gmail.com>
2023-04-30 10:38:22 +0300

    for deployment - test error - 2

--- backend/routes/profileRouters.js
+++ backend/routes/profileRouters.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { protect1 } = require('../middleWare/authMiddleware.js')
+const protect = require('../middleWare/authMiddleware.js')
 const { getUserProfile, editProfile } = require('../controller/profileController')
 
 // router.get('/getProfile', protect, getUserProfile)
 // router.put('/editProfile', protect, editProfile)
-protect1()
+
 module.exports = router
