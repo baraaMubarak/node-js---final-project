2b9ec53b16b167e6cf33cb3713d77cacb149739e
praam <praa.m.2002@gmail.com>
2023-05-04 21:25:55 +0300

    updates

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -7,5 +7,6 @@ const errorHandler = (err, req, res, next) => {
         details: err.details,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
       });
+
 }
 module.exports = errorHandler
