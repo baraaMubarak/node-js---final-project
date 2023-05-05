ed9e8ec03941ede6c3bd0431d2b91d5358e9d465
Nassem <nassemmubarak@gmail.com>
2023-05-05 17:51:49 +0300

    update validation phone numbar

--- backend/core_modules/validation/phoneNumberValidate.js
+++ backend/core_modules/validation/phoneNumberValidate.js
@@ -3,10 +3,10 @@ module.exports = (phoneNumber) => {
     if (phoneNumber === '') {
         return 'Please enter a phone number.'
     } else {
-        const regex = /^\+?[1-9]\d{1,14}$/; // Regular expression for a valid phone number
+        const regex = /^\+[0-9]{12}$/; // Regular expression for a valid phone number
         const isValidate = regex.test(phoneNumber);
         if (!isValidate) {
-            return "Phone number is invalid.";
+            return "Phone number is invalid. hent number +970592815701";
         } else {
             return null
         }
